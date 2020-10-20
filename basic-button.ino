bcb2d25fdf04b547938728dfd5047755b180aea0
Edward Li <edward@barnabasrobotics.com>
2020-10-19 19:41:59 -0700

    Create basic-button.ino

---
+++ basic-button.ino
@@ -0,0 +1,31 @@
+/*
+
+1) Flip the logic.  Press -> LED OFF, No Press -> LED ON
+1A) Do challenge 1 a second way
+2) Press -> Blink the LED, No Press -> LED off
+3) Press -> Blink fast, No Press -> Blink slowly
+
+*/
+
+
+void setup()
+{
+  pinMode(7,OUTPUT);
+  pinMode(2,INPUT);
+}
+
+
+void loop()
+{
+
+  //- if I press the button
+  if (digitalRead(2) == LOW) {
+  //- then turn the light on
+    digitalWrite(7,HIGH);
+  }
+  //- else turn the light off
+  else {
+    digitalWrite(7,LOW);
+  }
+
+}
