a660cccf1244bceacbb8286f7f5cf90cf859d64c
Edward Li <edward@barnabasrobotics.com>
2020-11-05 12:14:02 -0800

    Create rgb_tinkerCAD_blink.ino

---
+++ rgb_tinkerCAD_blink.ino
@@ -0,0 +1,20 @@
+  digitalWrite(3,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  delay(1000);
+  digitalWrite(3,HIGH);
+  delay(1000);
+
+  digitalWrite(3,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  delay(1000);
+  digitalWrite(5,HIGH);
+  delay(1000);
+
+  digitalWrite(3,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  delay(1000);
+  digitalWrite(6,HIGH);
+  delay(1000);
