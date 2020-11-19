9669cfcf7499f63843dfdfa878b90c646bc87f23
Edward Li <edward@barnabasrobotics.com>
2020-11-19 14:00:30 -0800

    Create for-loop-servo-backandforth.ino

---
+++ for-loop-servo-backandforth.ino
@@ -0,0 +1,46 @@
+/*
+Variables = Box
+Value depends on the LAST set value
+*/
+#include <Servo.h>
+
+Servo edMotor;
+int edBox;
+
+void setup() {
+  pinMode(9,OUTPUT);
+  edMotor.attach(9);
+  edBox = 180; 
+}
+
+void loop() {
+
+  edBox = 0; 
+  edMotor.write(edBox);
+  //delay(1000);
+
+    //- for loop
+  for (int i = 0; i < 4; i++) {
+    edBox = edBox + 45;
+    edMotor.write(edBox);
+    
+    
+    delay(1000);
+  }
+  
+  edBox = 180; 
+  for (int i=0; i < 4; i++) {
+    edBox = edBox + -45;
+    edMotor.write(edBox);
+    delay(1000);
+  }
+  
+  
+  /** Challenges 
+   *  1) What should we change (i < 2) to if we want it to just go to 135 and then come back.
+   *  2) If I want to change the speed so that it is faster, how I can I do that
+   *     by just changing 1 line of code
+   *  3) Can you write code using the for loop to reverse the motion.
+   */
+  
+}
