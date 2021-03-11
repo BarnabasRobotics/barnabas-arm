8926f91af9449917119b5875f2f9416334970b37
Edward Li <edward@barnabasrobotics.com>
2021-03-11 13:54:27 -0800

    Create S2E8-softlimits-base-extend-lift_withbutton.ino

---
+++ S2E8-softlimits-base-extend-lift_withbutton.ino
@@ -0,0 +1,67 @@
+#include <Servo.h>
+
+/*
+Challenge #1: Create soft limits for all three motors
+Challenge #2: Create program to cycle through all the min and max positions
+Challenge #3: Add the mid positions using the math formulas and program the kick to happen at the middle of the base motor.
+Challenge #4: Modify program so that the motion only happens if you press a button (4-pin button).
+*/
+
+Servo baseMotor;
+Servo extendMotor;
+Servo liftMotor;
+
+//- soft limits for motors
+int baseMotorMin = 0;
+int baseMotorMax = 180;
+int baseMotorMid = (baseMotorMax - baseMotorMin) / 2;
+
+int extendMotorMax = 150;
+int extendMotorMin = 80;
+int extendMotorMid = (extendMotorMax - extendMotorMin) / 2;
+
+int liftMotorMax = 180;
+int liftMotorMin = 90;
+int liftMotorMid = (liftMotorMax - liftMotorMin) / 2;
+
+
+void setup() {
+  
+  pinMode(2,INPUT_PULLUP);
+  baseMotor.attach(9);
+  extendMotor.attach(10);
+  liftMotor.attach(11);
+}
+
+void routine() {
+  baseMotor.write(baseMotorMin);
+  delay(1000);
+  baseMotor.write(baseMotorMax);
+  delay(1000);
+  baseMotor.write(baseMotorMid);
+  delay(1000);
+  
+  extendMotor.write(extendMotorMin);
+  delay(1000);
+  extendMotor.write(extendMotorMax);
+  delay(1000);
+  
+  liftMotor.write(liftMotorMin);
+  delay(1000);
+  liftMotor.write(liftMotorMax);
+  delay(1000);
+}
+
+void loop() {
+  
+  //- if I press the button
+  if (digitalRead(2)==LOW) {
+    //- do the routine
+    routine();
+  }
+  else {
+    //- don't do anything
+    
+  }
+
+}
