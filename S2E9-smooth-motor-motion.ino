c494a8eb29a74cb172988df170faafb96a4bba4a
Edward Li <edward@barnabasrobotics.com>
2021-03-22 22:46:20 -0700

    Update S2E9-smooth-motor-motion.ino

--- S2E9-smooth-motor-motion.ino
+++ S2E9-smooth-motor-motion.ino
@@ -1,11 +1,9 @@
 #include <Servo.h>
 
 /*
-Challenge #1: Move base motor slowly back and forth
-Challenge #2: Move extend motor slowly back and forth
-Challenge #3: Move lift motor slowly back and forth 
-Challenge #4: Move fast or slow based on button press
-Challenge #5: Cause motors to move slowly right when you press the button so that you don't need to wait.
+Challenge #1: Add home position function
+Challenge #2: Add emergency stop so that it stops right away
+Challenge #3: Add emergency stop so that it stops right away, and then starts again in 3 seconds
 */
 
 Servo baseMotor;
@@ -15,18 +13,18 @@ Servo liftMotor;
 //- soft limits for motors
 int baseMotorMin = 0;
 int baseMotorMax = 180;
-int baseMotorMid = (baseMotorMax - baseMotorMin) / 2;
+int baseMotorMid = ((baseMotorMax - baseMotorMin) / 2) + baseMotorMin;
 
 int extendMotorMax = 150;
 int extendMotorMin = 80;
-int extendMotorMid = (extendMotorMax - extendMotorMin) / 2;
+int extendMotorMid = ((extendMotorMax - extendMotorMin) / 2) + extendMotorMin;
 
 int liftMotorMax = 180;
 int liftMotorMin = 90;
-int liftMotorMid = (liftMotorMax - liftMotorMin) / 2;
+int liftMotorMid = ((liftMotorMax - liftMotorMin) / 2) + liftMotorMin;
 
 //- speed
-int speed = 0;
+int speed = 25;
 
 
 void setup() {
@@ -59,12 +57,13 @@ void routine() {
 
 void slowRoutine() {
   
-  
   baseMotor.write(baseMotorMin);
+  checkEmergencyStop();
   
   //- moves base motor CCW
   for (int i = baseMotorMin; i<baseMotorMax; i++) {
     baseMotor.write(i);
+    checkEmergencyStop();
     Serial.println(i);
     delay(speed);
   }
@@ -72,16 +71,19 @@ void slowRoutine() {
   //- moves base motor CW
   for (int i = baseMotorMax; i>baseMotorMin; i--) {
     baseMotor.write(i);
+    checkEmergencyStop();
     Serial.println(i);
     delay(speed);
   }
   
   //- extend motor code
   extendMotor.write(extendMotorMin);
+  checkEmergencyStop();
   
   //- moves extend motor out
   for (int i = extendMotorMin; i<extendMotorMax; i++) {
     extendMotor.write(i);
+    checkEmergencyStop();
     Serial.println(i);
     delay(speed);
   }
@@ -89,37 +91,56 @@ void slowRoutine() {
   //- moves extend motor in
   for (int i = extendMotorMax; i>extendMotorMin; i--) {
     extendMotor.write(i);
+    checkEmergencyStop();
     Serial.println(i);
     delay(speed);
   }
   
   
+  //- lift motor code
+  liftMotor.write(liftMotorMin);
+  checkEmergencyStop();
   
-  /*
+  //- moves lift motor out
+  for (int i = liftMotorMin; i<liftMotorMax; i++) {
+    liftMotor.write(i);
+    checkEmergencyStop();
+    Serial.println(i);
+    delay(speed);
+  }
   
-  baseMotor.write(baseMotorMin);
-  delay(1000);
-  baseMotor.write(baseMotorMax);
-  delay(1000);
-  baseMotor.write(baseMotorMid);
-  delay(1000);
+  //- moves lift motor in
+  for (int i = liftMotorMax; i>liftMotorMin; i--) {
+    liftMotor.write(i);
+    checkEmergencyStop();
+    Serial.println(i);
+    delay(speed);
+  }
   
-  */
 
+}
+
+void homePosition() {
+  baseMotor.write(baseMotorMid);
+  extendMotor.write(extendMotorMid);
+  liftMotor.write(liftMotorMid);
+  checkEmergencyStop();
+}
+
+void checkEmergencyStop() {
+  if (digitalRead(2)==LOW) {
+    while (true) {
+      //- do nothing
+    }
+  }
 }
 
 void loop() {
   
-  //- when I press the button
-  if (digitalRead(2)==LOW) {
-    speed = 50;
-    slowRoutine();
-  }
-  else {
-    speed = 0;
-    slowRoutine();
-  }
+  homePosition();
+
+  delay(1000);
   
-  
+  slowRoutine();
   
 }
